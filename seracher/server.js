f5d345f953ff70b5739fc86bb9a137ca8e09437f
axloz23 <alexbie220@gmail.com>
2026-05-08 22:31:38 +0300

    this is the search tool Enjoy

---
+++ seracher/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/search', (req, res) => {
+    const query = encodeURIComponent(req.body.q);
+    const engine = req.body.engine;
+
+    const routes = {
+        google: `https://www.google.com/search?q=${query}`,
+        bing: `https://www.bing.com/search?q=${query}`,
+        duckduckgo: `https://duckduckgo.com/?q=${query}`,
+        yahoo: `https://search.yahoo.com/search?p=${query}`
+    };
+
+    const target = routes[engine] || routes.google;
+    res.redirect(target);
+});
+
+app.listen(PORT, () => {
+    console.log(`Searcher running at http://localhost:${PORT}`);
+});
